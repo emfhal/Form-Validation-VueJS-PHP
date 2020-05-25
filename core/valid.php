3f10ab4fa12a8c9f0841189b13f4441b437f0e46
FHAL <emfhal@gmail.com>
2020-05-25 19:30:41 +0300

    Update valid.php

--- core/valid.php
+++ core/valid.php
@@ -1,8 +1,15 @@
 <?php
-
+$cols = array();
 if (!empty($_REQUEST['name'])) {
     $text = $_REQUEST['name'];
     $is_english = preg_match('/^[A-Za-z]+$/i', $text);
     $strlen = strlen($text);
-    if ($is_english and $strlen > 3) echo "success";
+    if ($is_english and $strlen > 3){
+        $cols['status'] = "success";
+    } else{
+        $cols['status'] = false;
+    }
 }
+
+header('Content-type:application/json;charset=utf-8');
+echo json_encode( $cols );
